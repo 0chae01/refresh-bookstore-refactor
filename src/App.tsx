f1a5e86819b5724b0412c05c2e77879851c0acb4
youngchae <010101chae@gmail.com>
2023-06-30 17:36:29 +0900

    chore: eslint, prettier 설정 및 react-router-dom 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className={`w-full h-[100vh] bg-point text-white`} >
-        ddd
-
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className={'w-full h-[100vh] bg-point text-white'}>hi</div>
+		</div>
+	);
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'point': '#1DC078',
-        'black': '#111111',
-        'dark-gray': '#7d7d7d',
-        'gray': '#bfbfbf',
-        'light-gray': '#e7e7e7',
-        'light-green': '#b7fbdc',
-        'dark-green': '#16a263',
-        'red': '#FF0000'
-        
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				point: '#1DC078',
+				black: '#111111',
+				'dark-gray': '#7d7d7d',
+				gray: '#bfbfbf',
+				'light-gray': '#e7e7e7',
+				'light-green': '#b7fbdc',
+				'dark-green': '#16a263',
+				red: '#FF0000',
+			},
+		},
+	},
+	plugins: [],
+};
