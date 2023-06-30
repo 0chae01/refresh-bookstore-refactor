1715fe8530a3f1ce1c151aadba44642b66aecc10
baansuk <baansuk_hwang@naver.com>
2023-06-30 12:17:51 +0900

    Chore: Tailwind Config(color)

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`w-full h-[100vh] bg-point text-white`} >
+        ddd
+
+      </div>
     </div>
   );
 }
