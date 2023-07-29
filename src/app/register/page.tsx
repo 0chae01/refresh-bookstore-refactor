c73ff50f4d7764d9b69446f3f68e77a1293a57e1
baansuk <baansuk_hwang@naver.com>
2023-07-29 16:30:50 +0900

    Feat: 회원가입 구현 및 주소팝업 약간 수정

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,11 @@
 "use client";
+import AlertIcon from "@/components/Common/Icons/AlertIcon";
 import CheckIcon from "@/components/Common/Icons/CheckIcon";
+import Post from "@/components/Common/Post";
+import { postCodePopupStore } from "@/stores";
 import autoHyphen from "@/utils/autoHyphen";
 import React, { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 
 const RegisterPage = () => {
   const [inputCheck, setInputCheck] = useState({
@@ -36,14 +40,34 @@ const RegisterPage = () => {
       isConfirmed: false,
     },
   });
+  const [popup, setPopup] = useRecoilState(
+    postCodePopupStore.postCodePopupState
+  );
+  const [address, setAddress] = useState({
+    address: "",
+    zonecode: "",
+  });
 
-  const emailInput = document.getElementById("email-input");
-  const nameInput = document.getElementById("name-input");
-  const passwordInput = document.getElementById("password-input");
+  const emailInput = document.getElementById("email-input") as HTMLInputElement;
+  const nameInput = document.getElementById("name-input") as HTMLInputElement;
+  const passwordInput = document.getElementById(
+    "password-input"
+  ) as HTMLInputElement;
   const passwordConfirmInput = document.getElementById(
     "password-confirm-input"
-  );
-  const phoneNumberInput = document.getElementById("phone-number-input");
+  ) as HTMLInputElement;
+  const phoneNumberInput = document.getElementById(
+    "phone-number-input"
+  ) as HTMLInputElement;
+  const addressZipcodeInput = document.getElementById(
+    "address-zipcode-input"
+  ) as HTMLInputElement;
+  const addressAddressInput = document.getElementById(
+    "address-address-input"
+  ) as HTMLInputElement;
+  const addressDetailInput = document.getElementById(
+    "address-detail-input"
+  ) as HTMLInputElement;
 
   useEffect(() => {
     if (inputCheck.passwordConfirm.value === "") {
@@ -218,8 +242,47 @@ const RegisterPage = () => {
     }
   };
 
+  const checkAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (addressZipcodeInput.value) {
+      addressZipcodeInput.style.borderBottomColor = "#1DC078";
+    } else {
+      addressZipcodeInput.style.borderBottomColor = "black";
+    }
+
+    if (addressAddressInput.value) {
+      addressAddressInput.style.borderBottomColor = "#1DC078";
+    } else {
+      addressAddressInput.style.borderBottomColor = "black";
+    }
+
+    if (addressDetailInput.value) {
+      addressDetailInput.style.borderBottomColor = "#1DC078";
+    } else {
+      addressDetailInput.style.borderBottomColor = "black";
+    }
+    if (
+      addressZipcodeInput.value &&
+      addressAddressInput.value &&
+      addressDetailInput.value
+    ) {
+      setInputCheck((prevState) => ({
+        ...prevState,
+        address: { ...prevState.address, status: "correct" },
+      }));
+    } else {
+      setInputCheck((prevState) => ({
+        ...prevState,
+        address: { ...prevState.address, status: "wrong" },
+      }));
+    }
+  };
+
+  const handlePostClick = () => {
+    setPopup(!popup);
+  };
+
   return (
-    <div className="w-full h-[80vh] flex flex-col justify-start items-center">
+    <div className="w-full h-auto flex flex-col justify-start items-center">
       <div className="w-[80%] h-[80px] mt-10 font-black text-3xl text-dark_green flex flex-col justify-center items-end ">
         회원가입
       </div>
@@ -243,6 +306,7 @@ const RegisterPage = () => {
               className="w-full h-[30px] border-b border-black text-xl outline-none"
               id="name-input"
               onChange={(e) => checkName(e)}
+              placeholder="이름을 입력해주세요."
             />
           </div>
         </div>
@@ -262,13 +326,17 @@ const RegisterPage = () => {
               id="email-input"
               className={`w-full h-[30px] border-b border-black text-xl outline-none`}
               onChange={(e) => checkEmail(e)}
+              placeholder="이메일을 입력해주세요."
             />
             <div className="text-xs text-red">
-              {inputCheck.email.status === "wrong"
-                ? "올바른 이메일을 입력해주세요."
-                : inputCheck.email.status === "exist"
-                ? "이미 존재하는 이메일입니다."
-                : ""}
+              {inputCheck.email.status === "wrong" ? (
+                <div className="text-xs text-red flex flex-row items-center">
+                  <AlertIcon width="15px" color="red" />
+                  올바른 형식의 이메일을 입력해주세요.
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </div>
@@ -289,11 +357,18 @@ const RegisterPage = () => {
               id="password-input"
               className={`w-full h-[30px] border-b border-black text-xl outline-none`}
               onChange={(e) => checkPassword(e)}
+              placeholder="비밀번호를 입력해주세요."
             />
             <div className="text-xs text-red">
-              {inputCheck.password.status === "wrong"
-                ? "비밀번호는 8~20자의 영문자와 숫자를 포함한 조합으로 만들어주세요."
-                : ""}
+              {inputCheck.password.status === "wrong" ? (
+                <div className="text-xs text-red flex flex-row items-center">
+                  <AlertIcon width="15px" color="red" />
+                  비밀번호는 8~20자의 영문자와 숫자를 포함한 조합으로
+                  입력해주세요.
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </div>
@@ -314,11 +389,17 @@ const RegisterPage = () => {
               id="password-confirm-input"
               className={`w-full h-[30px] border-b border-black text-xl outline-none`}
               onChange={(e) => checkPasswordConfirm(e)}
+              placeholder="비밀번호를 확인해주세요."
             />
             <div className="text-xs text-red">
-              {inputCheck.passwordConfirm.status === "wrong"
-                ? "비밀번호가 일치하지 않습니다."
-                : ""}
+              {inputCheck.passwordConfirm.status === "wrong" ? (
+                <div className="text-xs text-red flex flex-row items-center">
+                  <AlertIcon width="15px" color="red" />
+                  비밀번호가 일치하지 않습니다.
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </div>
@@ -338,15 +419,89 @@ const RegisterPage = () => {
               id="phone-number-input"
               className={`w-full h-[30px] border-b border-black text-xl outline-none`}
               onChange={(e) => checkPhoneNumber(e)}
+              placeholder="휴대폰 번호를 입력해주세요."
             />
-            <div className="text-xs text-red">
-              {inputCheck.phoneNumber.status === "wrong"
-                ? "올바른 번호를 입력해주세요."
-                : ""}
+            <div className="text-xs text-red flex flex-row items-center">
+              {inputCheck.phoneNumber.status === "wrong" ? (
+                <div className="text-xs text-red flex flex-row items-center">
+                  <AlertIcon width="15px" color="red" />
+                  올바른 번호를 입력해주세요.
+                </div>
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </div>
+        <div className="w-[60%] h-auto flex flex-row justify-around items-start mx-10 my-2">
+          {inputCheck.address.status === "correct" ? (
+            <label className="w-[25%] h-[30px] flex flex-row justify-start items-center text-point font-bold text-xl">
+              주소 <CheckIcon color="#1DC078" width="20px" />
+            </label>
+          ) : (
+            <label className="w-[25%] h-[30px] flex flex-row justify-start items-center font-semibold text-xl">
+              주소
+            </label>
+          )}
+
+          <div className="w-[65%] flex flex-col justify-start">
+            <div className="w-full flex flex-row justify-between">
+              <input
+                id="address-zipcode-input"
+                className={`w-[45%] h-[30px] border-b my-3 border-black text-xl outline-none`}
+                onChange={(e) => checkAddress(e)}
+                onClick={handlePostClick}
+                value={address.zonecode}
+                placeholder="우편번호"
+              />
+              <input
+                type="button"
+                className="w-[50%] h-[35px] rounded-md  my-2 bg-white border hover:bg-light_gray border-black text-lg font-bold"
+                onClick={handlePostClick}
+                value="우편번호 찾기"
+              />
+            </div>
+
+            <input
+              id="address-address-input"
+              className={`w-full h-[30px] border-b my-3 border-black text-xl outline-none`}
+              onChange={(e) => checkAddress(e)}
+              value={address.address}
+              onClick={handlePostClick}
+              placeholder="주소를 입력해주세요."
+            />
+            <input
+              id="address-detail-input"
+              className={`w-full h-[30px] border-b my-3 border-black text-xl outline-none`}
+              onChange={(e) => checkAddress(e)}
+              placeholder="상세주소를 입력해주세요."
+            />
+          </div>
+        </div>
+        <div className="w-[60%] h-[100px] flex flex-col justify-around items-start mx-10 my-2">
+          {inputCheck.name.status === "correct" &&
+          inputCheck.email.status === "correct" &&
+          inputCheck.password.status === "correct" &&
+          inputCheck.passwordConfirm.status === "correct" &&
+          inputCheck.phoneNumber.status === "correct" &&
+          inputCheck.address.status === "correct" ? (
+            <button
+              className="w-full h-[50px] rounded-lg  my-2 bg-point hover:bg-dark_green text-white text-lg font-extrabold"
+              type="submit"
+            >
+              가입하기
+            </button>
+          ) : (
+            <div
+              className="w-full h-[50px] rounded-lg  my-2 bg-gray text-white text-lg font-extrabold flex flex-col justify-center items-center"
+              type="submit"
+            >
+              가입하기
+            </div>
+          )}
+        </div>
       </form>
+      {popup && <Post address={address} setAddress={setAddress} />}
     </div>
   );
 };
